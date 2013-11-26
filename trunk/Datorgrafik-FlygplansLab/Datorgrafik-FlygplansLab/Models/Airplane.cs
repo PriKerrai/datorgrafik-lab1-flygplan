45f07fcf941d64718ba46792ea29719335d175a3
Martin.Kerrai.Nilsson@gmail.com <Martin.Kerrai.Nilsson@gmail.com@6cce6e42-d7f3-eeb3-eeda-d3f785f5f2b5>
2013-11-26 13:18:11 +0000

    FPS och Basic Camera is works.

---
+++ trunk/Datorgrafik-FlygplansLab/Datorgrafik-FlygplansLab/Models/Airplane.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace GrafikTest
+{
+    class Airplane
+    {
+
+        public int[] indices { get; set; }
+        public VertexPositionColor[] vertices { get; set; }
+
+        public void InitializeVertices()
+        {
+            //Initialize vertices
+            vertices = new VertexPositionColor[36];
+
+        }
+
+        public void InitializeIndices()
+        {
+            
+        }
+
+    }
+}
